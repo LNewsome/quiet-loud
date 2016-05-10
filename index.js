882d27ec21f1988ef42a20ae8be506e8a97ee7e8
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-05-10 11:23:04 -0400

    commit to master. port changed to 3000

--- index.js
+++ index.js
@@ -66,6 +66,6 @@ app.get("/:name", function(req, res){
   })
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("port listening on port 3000")
 });
